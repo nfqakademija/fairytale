c937ade26e76a63a59ba849bab9f93627e9661bf
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-08-13 22:06:11 +0300

    Added Core bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Fairytale\FrontendBundle\FairytaleFrontendBundle(),
+            new Fairytale\CoreBundle\FairytaleCoreBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Fairytale/CoreBundle/FairytaleCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Fairytale\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FairytaleCoreBundle extends Bundle
+{
+}
