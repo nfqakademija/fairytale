d8cb9f6306bf5b617f735126536051bb19747617
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-08-13 21:15:58 +0300

    Generated FrontendBundle

---
+++ src/Fairytale/FrontendBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fairytale\FrontendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('FairytaleFrontendBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Fairytale/FrontendBundle/FairytaleFrontendBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Fairytale\FrontendBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FairytaleFrontendBundle extends Bundle
+{
+}
