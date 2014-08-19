d0e4efb8a8fa5e2a74bb471877493af9e94753cd
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-08-19 22:39:54 +0300

    Added spec for Auth listener

--- src/Nfq/Fairytale/ApiBundle/EventListener/Authorization.php
+++ src/Nfq/Fairytale/ApiBundle/EventListener/Authorization.php
@@ -2,6 +2,12 @@
 
 namespace Nfq\Fairytale\ApiBundle\EventListener;
 
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+
 class Authorization
 {
+    public function onKernelController(FilterControllerEvent $event)
+    {
+        // TODO: write logic here
+    }
 }
--- src/Nfq/Fairytale/ApiBundle/spec/Nfq/Fairytale/ApiBundle/EventListener/AuthorizationSpec.php
+++ src/Nfq/Fairytale/ApiBundle/spec/Nfq/Fairytale/ApiBundle/EventListener/AuthorizationSpec.php
@@ -4,11 +4,17 @@ namespace spec\Nfq\Fairytale\ApiBundle\EventListener;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 
 class AuthorizationSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('Nfq\Fairytale\ApiBundle\EventListener\Authorization');
+    }
+
+    function it_should_handle_event(FilterControllerEvent $event)
+    {
+        $this->onKernelController($event)->shouldBe(null);
     }
 }
