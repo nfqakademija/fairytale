35d539781c3f59a3d73c9cb3559f9deea5df46b8
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-08-26 23:37:01 +0300

    Behat conf fix

--- .vagrant/manifests/default.pp
+++ .vagrant/manifests/default.pp
@@ -1,6 +1,7 @@
 $serverName = "fairytale.dev"
 $phpTimeZone = "Europe/Vilnius"
 $packages = ['vim', 'curl']
+$hostnames = ['fairytale.dev', 'test.fairytale.dev']
 
 # Ensure the time is accurate, reducing the possibilities of apt repositories
 # failing for invalid certificates
@@ -79,6 +80,14 @@ if ! defined(Package['augeas-tools']) {
   }
 }
 
+### HOSTS ###
+
+class { 'hosts':
+    dynamic_mode => true,
+    dynamic_ip    => '127.0.0.1',
+    dynamic_alias => $hostnames,
+}
+
 ### NGINX ###
 
 class { 'nginx': }
@@ -118,6 +127,42 @@ nginx::resource::location { "${serverName}-php":
     'fastcgi_param  '         => $script_filename,
     'fastcgi_pass'            => 'unix:/var/run/php5-fpm.sock',
     'fastcgi_index'           => 'app_dev.php',
+    'include'                 => 'fastcgi_params'
+  },
+  notify              => Class['nginx::service'],
+}
+
+# tests environment for behat
+
+nginx::resource::vhost { "test.${serverName}":
+  ensure       => present,
+  server_name  => [
+    "test.${serverName}",
+  ],
+  index_files  => [
+    'app_test.php',
+  ],
+  listen_port  => 80,
+  www_root     => "/var/www/${serverName}/web/",
+  try_files    => ['$uri', '$uri/', '/app_test.php?$args'],
+}
+
+nginx::resource::location { "test.${serverName}-php":
+  ensure              => 'present',
+  index_files         => [
+    'app_test.php',
+  ],
+  vhost               => "test.${serverName}",
+  location            => '~ \.php$',
+  proxy               => undef,
+  www_root            => "/var/www/${serverName}/web/",
+  location_cfg_append => {
+    'fastcgi_split_path_info' => '^(.+\.php)(/.+)$',
+    'fastcgi_param'           => 'PATH_INFO $fastcgi_path_info',
+    'fastcgi_param '          => $path_translated,
+    'fastcgi_param  '         => $script_filename,
+    'fastcgi_pass'            => 'unix:/var/run/php5-fpm.sock',
+    'fastcgi_index'           => 'app_test.php',
     'include'                 => 'fastcgi_params'
   },
   notify              => Class['nginx::service'],
