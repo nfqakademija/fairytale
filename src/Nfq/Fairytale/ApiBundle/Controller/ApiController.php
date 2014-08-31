92bc9b062189c61b8ace252e0636b9f28527e19e
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-08-31 16:33:04 +0300

    Cleaned up controller

--- src/Nfq/Fairytale/ApiBundle/Controller/ApiController.php
+++ src/Nfq/Fairytale/ApiBundle/Controller/ApiController.php
@@ -13,61 +13,6 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ApiController implements ApiControllerInterface
 {
-    /** @var  DatasourceFactory */
-    protected $factory;
-
-    /** @var  int */
-    protected $defaultIndexSize;
-
-    /** @var  ActionManager */
-    protected $actionManager;
-
-    /** @var  ResourceResolver */
-    protected $resolver;
-
-    /**
-     * @param DatasourceFactory $factory
-     */
-    public function setDatasourceFactory(DatasourceFactory $factory)
-    {
-        $this->factory = $factory;
-    }
-
-    /**
-     * @param int $defaultCollectionSize
-     */
-    public function setDefaultIndexSize($defaultCollectionSize)
-    {
-        $this->defaultIndexSize = $defaultCollectionSize;
-    }
-
-    /**
-     * @param ActionManager $actionManager
-     */
-    public function setActionManager(ActionManager $actionManager)
-    {
-        $this->actionManager = $actionManager;
-    }
-
-    /**
-     * @param ResourceResolver $resolver
-     */
-    public function setResolver(ResourceResolver $resolver)
-    {
-        $this->resolver = $resolver;
-    }
-
-    public function indexAction(Request $request, $resource)
-    {
-        return [
-            $this->factory->create($this->resolver->resolve($resource))->index(
-                $request->query->get('limit', $this->defaultIndexSize),
-                $request->query->get('offset', 0)
-            ),
-            200
-        ];
-    }
-
     public function customAction(Request $request, $resource, $action, $identifier = null)
     {
         switch (true) {
