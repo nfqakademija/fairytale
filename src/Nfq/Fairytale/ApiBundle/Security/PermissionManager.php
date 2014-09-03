d43268c02a0e76f9ad0186a4efaa1323130d6809
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-09-03 22:37:08 +0300

    Added new PermissionManager

---
+++ src/Nfq/Fairytale/ApiBundle/Security/PermissionManager.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Nfq\Fairytale\ApiBundle\Security;
+
+class PermissionManager
+{
+    /** @var  array */
+    protected $acl;
+
+    /**
+     * @param array $acl
+     */
+    public function setAcl($acl)
+    {
+        $this->acl = $acl;
+    }
+
+    public function isReadable($path, $action, array $roles)
+    {
+        return $this->isAllowed($path, $action, $roles, 'read');
+    }
+
+    public function isWritable($path, $action, array $roles)
+    {
+        return $this->isAllowed($path, $action, $roles, 'write');
+    }
+
+    /**
+     * @param        $path
+     * @param        $action
+     * @param array  $roles
+     * @param string $operation
+     * @return bool
+     */
+    private function isAllowed($path, $action, array $roles, $operation)
+    {
+        $hasField = false !== strpos($path, ':');
+        list($fqcn, $field) = $hasField ? explode(':', $path) : [$path, null];
+        $parts = explode('\\', $fqcn);
+        $class = array_pop($parts);
+        $namespace = join('\\', $parts);
+
+        if ($hasField) {
+            $requiredRole = @$this->acl[$namespace][$class][$action][$operation][$field];
+        } else {
+            $requiredRole = @$this->acl[$namespace][$class][$action][$operation];
+        }
+
+        return [] != array_intersect(is_array($requiredRole) ? $requiredRole : [$requiredRole], $roles);
+    }
+}
