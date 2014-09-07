7c4b103ce3dee0f23e4dcff1e391ee6debb38dd4
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-09-07 08:40:08 +0300

    Cleanup

--- src/Nfq/Fairytale/ApiBundle/spec/Nfq/Fairytale/ApiBundle/EventListener/AuthorizationListenerSpec.php
+++ src/Nfq/Fairytale/ApiBundle/spec/Nfq/Fairytale/ApiBundle/EventListener/AuthorizationListenerSpec.php
@@ -6,31 +6,13 @@ use JMS\Serializer\Serializer;
 use JMS\Serializer\SerializerInterface;
 use Nfq\Fairytale\ApiBundle\Actions\ActionInterface;
 use Nfq\Fairytale\ApiBundle\Actions\ActionManager;
-use Nfq\Fairytale\ApiBundle\Actions\ActionResult;
 use Nfq\Fairytale\ApiBundle\Controller\ApiControllerInterface;
 use Nfq\Fairytale\ApiBundle\EventListener\AuthorizationListener;
-use Nfq\Fairytale\ApiBundle\Helper\ActionResolver;
-use Nfq\Fairytale\ApiBundle\Helper\RawContentSerializer;
 use Nfq\Fairytale\ApiBundle\Helper\ResourceResolver;
-use Nfq\Fairytale\ApiBundle\Security\CredentialStore;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\Debug\Exception\ContextErrorException;
-use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
-use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
-use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
-use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Role\Role;
-use Symfony\Component\Security\Core\Role\RoleInterface;
-use Symfony\Component\Security\Core\SecurityContext;
-use Symfony\Component\Security\Core\SecurityContextInterface;
 
 /**
  * @mixin AuthorizationListener
