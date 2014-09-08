6a846cfc2b1d399be71aa4a8dbc3776c52013244
Darius Kasiulevičius <darius.kasiulevicius@nfq.lt>
2014-09-08 22:14:19 +0300

    Added page header

--- src/Nfq/Fairytale/FrontendBundle/Resources/public/scripts/Controller/UserController.js
+++ src/Nfq/Fairytale/FrontendBundle/Resources/public/scripts/Controller/UserController.js
@@ -1,5 +1,5 @@
 angular
-    .module('controller.User', [])
+    .module('fairytale.controller.user', [])
     .controller('userController', function ($scope, $http) {
         $http.get('api/user/2').success(function (data) {
             $scope.user = data;
