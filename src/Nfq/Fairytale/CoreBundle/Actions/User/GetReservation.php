14894812bccac62e5a70c1df74452f9a313bc578
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-09-10 22:46:53 +0300

    Added user reservation endpoint - /api/user/1/reservation     #9

---
+++ src/Nfq/Fairytale/CoreBundle/Actions/User/GetReservation.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Nfq\Fairytale\CoreBundle\Actions\User;
+
+use Nfq\Fairytale\ApiBundle\Actions\ActionResult;
+use Nfq\Fairytale\ApiBundle\Actions\Instance\BaseInstanceAction;
+use Nfq\Fairytale\ApiBundle\DataSource\DataSourceInterface;
+use Nfq\Fairytale\ApiBundle\DataSource\Factory\DataSourceFactory;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
+
+class GetReservation extends BaseInstanceAction
+{
+    const NAME = 'user.reservation';
+
+    /** @var  DataSourceFactory */
+    protected $factory;
+
+    /**
+     * Performs the action
+     *
+     * @param Request             $request
+     * @param DataSourceInterface $resource
+     * @param string              $identifier
+     * @return ActionResult
+     */
+    public function execute(Request $request, DataSourceInterface $resource, $identifier)
+    {
+        $resource = $this->factory->create('Nfq\Fairytale\CoreBundle\Entity\Reservation');
+
+        $reservations = $resource->query(['user' => $identifier]);
+        return ActionResult::instance(200, ['id' => count($reservations) ? $reservations[0]->getId() : null]);
+    }
+
+    /**
+     * @param DataSourceFactory $factory
+     */
+    public function setFactory(DataSourceFactory $factory)
+    {
+        $this->factory = $factory;
+    }
+}
---
+++ src/Nfq/Fairytale/CoreBundle/DataFixtures/ORM/LoadReservationData.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Nfq\Fairytale\CoreBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManager;
+use Nfq\Fairytale\CoreBundle\Entity\Book;
+use Nfq\Fairytale\CoreBundle\Entity\Reservation;
+
+class LoadReservationData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 5;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        /** @var EntityManager $em */
+        $em = $manager;
+        $example = new Reservation();
+        $example->setUser($em->getPartialReference('Nfq\Fairytale\CoreBundle\Entity\User', 1));
+        $example->setBook($em->getPartialReference('Nfq\Fairytale\CoreBundle\Entity\Book', 1));
+        $example->setStatus('pending');
+        $example->setCreatedAt(new \DateTime());
+
+        $manager->persist($example);
+
+        $manager->flush();
+    }
+}
