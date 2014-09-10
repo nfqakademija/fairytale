e07c9e328d3b2ac6497dfc6d1a8c1ca390e8a8c9
Darius Kasiulevičius <darius.kasiulevicius@nfq.lt>
2014-09-10 23:01:10 +0300

    Fixed dependency injection load

--- src/Nfq/Fairytale/CoreBundle/DependencyInjection/NfqFairytaleCoreExtension.php
+++ src/Nfq/Fairytale/CoreBundle/DependencyInjection/NfqFairytaleCoreExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class FairytaleCoreExtension extends Extension
+class NfqFairytaleCoreExtension extends Extension
 {
     /**
      * {@inheritDoc}
