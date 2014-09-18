1c2e774c313744223856b28ea3246bdf3ce20367
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-09-18 19:45:10 +0300

    Replaced user read generic action with custom one

---
+++ src/Nfq/Fairytale/CoreBundle/Actions/User/GetDetails.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Nfq\Fairytale\CoreBundle\Actions\User;
+
+use Nfq\Fairytale\ApiBundle\Actions\ActionResult;
+use Nfq\Fairytale\ApiBundle\Actions\Instance\BaseInstanceAction;
+use Nfq\Fairytale\ApiBundle\DataSource\DataSourceInterface;
+use Nfq\Fairytale\CoreBundle\Entity\User;
+use Nfq\Fairytale\CoreBundle\Util\ImageResolvingInterface;
+use Nfq\Fairytale\CoreBundle\Util\ImageResolvingTrait;
+use Symfony\Component\HttpFoundation\Request;
+
+class GetDetails extends BaseInstanceAction implements ImageResolvingInterface
+{
+    use ImageResolvingTrait;
+
+    const NAME = 'user.details';
+
+    /**
+     * Performs the action
+     *
+     * @param Request             $request
+     * @param DataSourceInterface $resource
+     * @param string              $identifier
+     * @return ActionResult
+     */
+    public function execute(Request $request, DataSourceInterface $resource, $identifier)
+    {
+        /** @var User $book */
+        $user = $resource->read($identifier);
+
+        $ref = new \ReflectionObject($user);
+        $props = $ref->getProperties();
+
+        $raw = [];
+        foreach ($props as $prop) {
+            $prop->setAccessible(true);
+            $raw[$prop->getName()] = $prop->getValue($user);
+        }
+
+        $raw['image'] = $this->resolveImages($user->getImage()->getFileName());
+
+        return ActionResult::instance(200, $raw);
+    }
+}
