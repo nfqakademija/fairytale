edcecfb4f057343386603346249620aecd988836
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-09-20 08:43:20 +0300

    Added category "All"

---
+++ src/Nfq/Fairytale/CoreBundle/Actions/Category/GetAll.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Nfq\Fairytale\CoreBundle\Actions\Category;
+
+use Im0rtality\ApiBundle\Actions\ActionResult;
+use Im0rtality\ApiBundle\Actions\Collection\BaseCollectionAction;
+use Im0rtality\ApiBundle\DataSource\DataSourceInterface;
+use Im0rtality\ApiBundle\DataSource\Factory\DataSourceFactory;
+use Nfq\Fairytale\CoreBundle\Actions\Book\BookHelper;
+use Nfq\Fairytale\CoreBundle\Entity\Book;
+use Nfq\Fairytale\CoreBundle\Util\ImageResolvingInterface;
+use Nfq\Fairytale\CoreBundle\Util\ImageResolvingTrait;
+use Symfony\Component\HttpFoundation\Request;
+
+class GetAll extends BaseCollectionAction implements ImageResolvingInterface
+{
+    use ImageResolvingTrait;
+
+    const NAME = 'category.all';
+
+    /** @var  DataSourceFactory */
+    private $factory;
+
+    /**
+     * @param DataSourceFactory $factory
+     */
+    public function setFactory(DataSourceFactory $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    /**
+     * Performs the action
+     *
+     * @param Request             $request
+     * @param DataSourceInterface $resource
+     * @return ActionResult
+     */
+    public function execute(Request $request, DataSourceInterface $resource)
+    {
+        $resource = $this->factory->create('Nfq\Fairytale\CoreBundle\Entity\Book');
+        $books = $resource->index();
+        if ($books instanceof \Traversable) {
+            $books = iterator_to_array($books);
+        }
+
+        return ActionResult::instance(
+            200,
+            [
+                'id'    => 'all',
+                'title' => 'Visos',
+                'books' =>
+                    array_map(
+                        function (Book $book) {
+                            return BookHelper::toRaw($book, [$this, 'resolveImages']);
+                        },
+                        $books
+                    )
+            ]
+        );
+    }
+}
--- src/Nfq/Fairytale/CoreBundle/Features/API/Category.feature
+++ src/Nfq/Fairytale/CoreBundle/Features/API/Category.feature
@@ -96,3 +96,33 @@ Feature: API - Category
             | books.0.image.book_small   | string  |
             | books.0.image.book_tiny    | string  |
             | books.0.status             | string  |
+
+    Scenario: It should be possible to get category details
+        Given I am authenticated as "user"
+        When I send a GET request to "/api/category/all"
+        Then the response code should be 200
+        And I should get JSON response with following format:
+            | field                      | type    |
+            | id                         | string  |
+            | title                      | string  |
+            | books                      | array   |
+            | books.0.id                 | integer |
+            | books.0.title              | string  |
+            | books.0.description        | string  |
+            | books.0.pages              | integer |
+            | books.0.publisher          | string  |
+            | books.0.isbn               | string  |
+            | books.0.cover              | string  |
+            | books.0.language           | string  |
+            | books.0.categories         | array   |
+            | books.0.categories.0.id    | integer |
+            | books.0.categories.0.title | string  |
+            | books.0.authors            | array   |
+            | books.0.authors.0.id       | integer |
+            | books.0.authors.0.name     | string  |
+            | books.0.image              | array   |
+            | books.0.image.book_big     | string  |
+            | books.0.image.book_medium  | string  |
+            | books.0.image.book_small   | string  |
+            | books.0.image.book_tiny    | string  |
+            | books.0.status             | string  |
