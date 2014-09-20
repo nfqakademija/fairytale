a81eadfeedb6de57a8b9512d692d038b1e31f727
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-09-20 10:40:51 +0300

    Cleanup book/details action

--- src/Nfq/Fairytale/CoreBundle/Actions/Book/GetDetails.php
+++ src/Nfq/Fairytale/CoreBundle/Actions/Book/GetDetails.php
@@ -6,10 +6,10 @@ use Im0rtality\ApiBundle\Actions\ActionResult;
 use Im0rtality\ApiBundle\Actions\Instance\BaseInstanceAction;
 use Im0rtality\ApiBundle\DataSource\DataSourceInterface;
 use Nfq\Fairytale\CoreBundle\Entity\Book;
-use Nfq\Fairytale\CoreBundle\Entity\Category;
 use Nfq\Fairytale\CoreBundle\Util\ImageResolvingInterface;
 use Nfq\Fairytale\CoreBundle\Util\ImageResolvingTrait;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class GetDetails extends BaseInstanceAction implements ImageResolvingInterface
 {
@@ -30,30 +30,10 @@ class GetDetails extends BaseInstanceAction implements ImageResolvingInterface
         /** @var Book $book */
         $book = $resource->read($identifier);
 
-        // convert Book object to assoc array so we can inject more data
-        $ref = new \ReflectionObject($book);
-        $props = $ref->getProperties();
-
-        $raw = [];
-        foreach ($props as $prop) {
-            $prop->setAccessible(true);
-            $raw[$prop->getName()] = $prop->getValue($book);
+        if (!$book) {
+            throw new NotFoundHttpException();
         }
 
-        $raw['categories'] =
-            $book->getCategories()->map(function (Category $category) {
-                return [
-                    'id'    => $category->getId(),
-                    'title' => $category->getTitle(),
-                ];
-            })->toArray();
-
-
-        $raw['image'] = $this->resolveImages($book->getImage()->getFileName());
-
-        $raw = array_replace($raw, [
-            'status' => 'unknown', // 'available', 'reserved', 'taken',
-        ]);
-        return ActionResult::instance(200, $raw);
+        return ActionResult::instance(200, BookHelper::toRaw($book, [$this, 'resolveImages']));
     }
 }
