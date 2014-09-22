0e4c9244e2455b560a6498d84c60bdcc495f3a55
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-09-22 22:49:12 +0300

    Fixed insta search     Closes #22, closes #21

--- src/Nfq/Fairytale/FrontendBundle/Resources/assets/scripts/Search/SearchController.js
+++ src/Nfq/Fairytale/FrontendBundle/Resources/assets/scripts/Search/SearchController.js
@@ -2,13 +2,36 @@ angular
     .module('Fairytale.Search.Controller', [
         'Fairytale.SearchResult.Model'
     ])
-    .controller('SearchController', ['$scope', 'SearchResult', function ($scope, SearchResult) {
-        $scope.query = null;
+    .controller('SearchController', [
+        '$scope', '$timeout', 'SearchResult', function ($scope, $timeout, SearchResult) {
+            $scope.query = null;
+            $scope.pause = 500;
+            $scope.results = [];
+            $scope.searchTimer = null;
 
-        $scope.search = function() {
-            if ($scope.results && $scope.results.$object) {
-                $scope.results.$object.reject();
-            }
-            $scope.results =  !$scope.query ? [] : SearchResult().getList({q: $scope.query}).$object;
-        };
-    }]);
+            $scope.onTimerComplete = function () {
+                $scope.searchTimer = null;
+                $scope.results = !$scope.query ? [] : SearchResult().getList({q: $scope.query}).$object;
+            };
+
+            $scope.cancelTimer = function () {
+                if ($scope.searchTimer) {
+                    $timeout.cancel($scope.searchTimer);
+                    $scope.searchTimer = null;
+                }
+            };
+
+            $scope.search = function () {
+                $scope.cancelTimer();
+                $scope.searchTimer = $timeout($scope.onTimerComplete, $scope.pause);
+            };
+
+            $scope.onResultClick = function () {
+                $scope.cancelTimer();
+                $timeout(function () {
+                    $scope.query = null;
+                    $scope.results = [];
+                });
+            };
+        }
+    ]);
