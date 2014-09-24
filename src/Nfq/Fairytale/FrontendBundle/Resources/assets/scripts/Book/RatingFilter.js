c9a2765f799185d576323753f3805a9c79b0b1fc
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-09-24 08:11:34 +0300

    Added rating for book details page

--- src/Nfq/Fairytale/FrontendBundle/Resources/assets/scripts/Book/RatingFilter.js
+++ src/Nfq/Fairytale/FrontendBundle/Resources/assets/scripts/Book/RatingFilter.js
@@ -3,10 +3,20 @@ angular
     .filter('rating', [
         function () {
             return function (book, precision) {
+                if (!book.ratings) {
+                    return '?';
+                }
+                var sum, coef;
+
                 precision = precision || 0;
-                return Math.round(_.reduce(book.ratings, function (sum, rating) {
+
+                coef = Math.pow(10, precision);
+
+                sum = _.reduce(book.ratings, function (sum, rating) {
                     return sum + rating.value;
-                }, 0).valueOf() / book.ratings.length * Math.pow(10, precision)) / Math.pow(10, precision);
+                }, 0).valueOf();
+
+                return Math.round(sum / book.ratings.length * coef) / coef;
             };
         }
     ]);
