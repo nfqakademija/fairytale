e7fc35011dea4657f746d16a198ce61eb8052689
Darius Kasiulevičius <darius.kasiulevicius@nfq.lt>
2014-09-30 22:25:34 +0300

    Created book status algorithm

--- src/Nfq/Fairytale/CoreBundle/Actions/Book/BookHelper.php
+++ src/Nfq/Fairytale/CoreBundle/Actions/Book/BookHelper.php
@@ -6,6 +6,7 @@ namespace Nfq\Fairytale\CoreBundle\Actions\Book;
 use Nfq\Fairytale\CoreBundle\Entity\Author;
 use Nfq\Fairytale\CoreBundle\Entity\Book;
 use Nfq\Fairytale\CoreBundle\Entity\Category;
+use Nfq\Fairytale\CoreBundle\Entity\Reservation;
 use Nfq\Fairytale\CoreBundle\Util\Doctrine;
 
 class BookHelper
@@ -30,7 +31,8 @@ class BookHelper
                     'id'   => $author->getId(),
                     'name' => $author->getName(),
                 ];
-            })->toArray();
+            }
+        )->toArray();
 
         $raw['image'] = $imageResolver($book->getImage()->getFileName());
 
@@ -39,6 +41,45 @@ class BookHelper
 
     private static function getBookStatus(Book $book)
     {
+        $reservations = $book->getReservations();
+        /** @var Reservation $reservation */
+        $dates = ['reserved' => [0], 'taken' => [0], 'returned' => [0]];
+        foreach ($reservations as $reservation) {
+            if ($reservation->getReturnedAt()) {
+                if ($dates['returned'][0] < $reservation->getReturnedAt()->getTimestamp()) {
+                    $dates['returned'] = [
+                        $reservation->getReturnedAt()->getTimestamp(),
+                        $reservation->getUser()->getId(),
+                        $reservation->getTakenAt()->getTimestamp(),
+                    ];
+                }
+            } elseif ($reservation->getTakenAt()) {
+                $dates['taken'] = [
+                    max($dates['taken'][0], $reservation->getTakenAt()->getTimestamp()),
+                    $reservation->getUser()->getId(),
+                ];
+            } elseif ($reservation->getCreatedAt()) {
+                $dates['reserved'] = [
+                    max($dates['reserved'][0], $reservation->getCreatedAt()->getTimestamp()),
+                    $reservation->getUser()->getId(),
+                ];
+            }
+        }
+
+        if ($dates['taken'][0] > $dates['returned'][0]) {
+            if ($dates['reserved'][0] > $dates['taken'][0]) {
+                return 'reserved';
+            }
+            return 'taken';
+        } elseif ($dates['taken'][0] < $dates['returned'][0]) {
+            if ($dates['reserved'][0] > $dates['returned'][2]) {
+                return 'reserved';
+            }
+            return 'available';
+        } elseif ($dates['reserved'][0] != 0) {
+            return 'reserved';
+        }
+
         return 'unknown';
     }
-} 
+}
