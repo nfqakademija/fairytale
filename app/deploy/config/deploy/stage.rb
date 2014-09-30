19af5225256de06827b83968f3aac75c65a6cbed
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-09-30 18:24:44 +0300

    Fixed deployment script

--- app/deploy/config/deploy/stage.rb
+++ app/deploy/config/deploy/stage.rb
@@ -1,5 +1,7 @@
 set :stage, :staging
-server 'www.projektai.nfqakademija.lt:22129', user: 'fairytale', roles: %w{web}
+set :password, ask('Server password:', nil)
+
+server 'www.projektai.nfqakademija.lt:22129', user: 'fairytale', password: fetch(:password), roles: %w{web}
 
 set :deploy_to, '/home/fairytale/public_html/'
 set :tmp_dir, '/tmp/fairytale'
